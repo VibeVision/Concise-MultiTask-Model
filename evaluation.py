6b9bfa93137d758c8a055e36d2af6bc6c435fae0
VibeVision <VibeVisio80@proton.me>
2020-06-28 06:03:09 +0000

    Testing webhook

--- evaluation.py
+++ evaluation.py
@@ -18,4 +18,11 @@ from models.scene_graph import *
 from models.surgicalDataset import *
 from models.segmentation_model import get_gcnet  # for the get_gcnet function
 
-from utils.scene_graph_eval_matrix impor
+from utils.scene_graph_eval_matrix import *
+from utils.segmentation_eval_matrix import *  # SegmentationLoss and Eval code
+
+from tabulate import tabulate
+
+import torch.multiprocessing as mp
+import torch.distributed as dist
+from torch.nn.parallel import Distr
