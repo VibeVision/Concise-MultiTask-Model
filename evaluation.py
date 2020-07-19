b8c553519191f15b72e30c5d339e4881da0fd0d0
VibeVision <VibeVisio80@proton.me>
2020-07-19 06:27:49 +0000

    0.19.0

--- evaluation.py
+++ evaluation.py
@@ -118,4 +118,11 @@ def model_eval(model, validation_dataloader, nclass=8):
         seg_img = data['img']
         seg_masks = data['mask']
         img_loc = data['img_loc']
-        node_num = data
+        node_num = data['node_num']
+        roi_labels = data['roi_labels']
+        det_boxes = data['det_boxes']
+        edge_labels = data['edge_labels']
+        spatial_feat = data['spatial_feat']
+        word2vec = data['word2vec']
+        
+        spatia
