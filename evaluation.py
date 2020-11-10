3c65b84c7a07f9892777e22191d69f39f779345e
VibeVision <VibeVisio80@proton.me>
2020-11-10 10:21:56 +0000

    doc -> docs

--- evaluation.py
+++ evaluation.py
@@ -263,4 +263,8 @@ if __name__ == "__main__":
     parser.add_argument('--multi_attn',         type=bool,      default = False)
     parser.add_argument('--diff_edge',          type=bool,      default = False)
     if model_type == 'mtl-t3' or model_type == 'amtl-t3':
-        pars
+        parser.add_argument('--global_feat',        type=int,       default = 128)
+    else:
+        parser.add_argument('--global_feat',        type=int,       default = 0)
+    # data_processing
+    parser.add_argument('--sampler',    
