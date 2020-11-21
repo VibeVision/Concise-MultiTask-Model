7f3dbdee833c5c259341d96694b5bd3af9f36ade
VibeVision <VibeVisio80@proton.me>
2020-11-21 20:32:04 +0000

    Silence warnings

--- model_train.py
+++ model_train.py
@@ -1,4 +1,18 @@
 '''
 Project         : Global-Reasoned Multi-Task Surgical Scene Understanding
 Lab             : MMLAB, National University of Singapore
-contributors    : Lalithkumar Seenivasan, Sai
+contributors    : Lalithkumar Seenivasan, Sai Mitheran, Mobarakol Islam, Hongliang Ren
+'''
+
+import os
+import time
+
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch.ut
