676c81a51626959a2df8a4bedb6b38e37b53368a
VibeVision <VibeVisio80@proton.me>
2021-01-01 21:46:08 +0000

    Add debugging information

--- model_train.py
+++ model_train.py
@@ -147,4 +147,7 @@ def model_eval(args, model, validation_dataloader):
 
     # Graph evaluation
     scene_graph_total_acc = scene_graph_total_acc / scene_graph_edge_count
-    scene_graph_total_loss = scene_graph_total_loss / len(
+    scene_graph_total_loss = scene_graph_total_loss / len(validation_dataloader)
+    scene_graph_logits_all = torch.cat(scene_graph_logits_list).cuda()
+    scene_graph_labels_all = torch.cat(scene_graph_labels_list).cuda()
+    scene_graph_logits_all = F.softmax(scene_graph_logits_all
