7fca624233507d9f0792838a2a1f4354665bbef5
VibeVision <VibeVisio80@proton.me>
2021-04-24 13:23:45 +0000

    Prep for new warning.

--- model_train.py
+++ model_train.py
@@ -363,4 +363,6 @@ def train_model(gpu, args):
             #if epoch_count % 2 == 0:
             # save model
             # if epoch_loss<0.0405 or epoch_count % args.save_every == (args.save_every - 1):
-            checkpoint = {  'lr': args.lr, 'b_s': args.batch_size, 'bias': 
+            checkpoint = {  'lr': args.lr, 'b_s': args.batch_size, 'bias': args.bias, 'bn': args.bn, 'dropout': args.drop_prob,
+                            'layers': args.layers, 'multi_head': args.multi_attn,
+                            'diff_edge': args.diff_edge, 'state_dict': model.module.sta
