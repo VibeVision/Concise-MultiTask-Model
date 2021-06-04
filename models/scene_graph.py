766959acadf8931b85c3490324376b04cf97c53d
VibeVision <VibeVisio80@proton.me>
2021-06-04 15:19:07 +0000

    Add version file

---
+++ models/scene_graph.py
@@ -0,0 +1,14 @@
+
+'''
+Project         : Global-Reasoned Multi-Task Surgical Scene Understanding
+Lab             : MMLAB, National University of Singapore
+contributors    : Lalithkumar Seenivasan, Sai Mitheran, Mobarakol Islam, Hongliang Ren
+Note            : Code adopted and modified from Visual-Semantic Graph Attention Networks and Dual attention network for scene segmentation
+                        Visual-Semantic Graph Network:
+                        @article{liang2020visual,
+                          title={Visual-Semantic Graph Attention Networks for Human-Object Interaction Detection},
+                          author={Liang, Zhijun and Rojas, Juan and Liu, Junfa and Guan, Yisheng},
+                          journal={arXiv preprint arXiv:2001.02302},
+                          year={2020}
+                        }
+'''
