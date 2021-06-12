aaa47d2741e78b0e5e32e7ee4f290d1eb7457b77
VibeVision <VibeVisio80@proton.me>
2021-06-12 12:34:13 +0000

    Added Danish translation

--- models/surgicalDataset.py
+++ models/surgicalDataset.py
@@ -1,2 +1,5 @@
 '''
-Project   
+Project         : Global-Reasoned Multi-Task Surgical Scene Understanding
+Lab             : MMLAB, National University of Singapore
+contributors    : Lalithkumar Seenivasan, Sai Mitheran, Mobarakol Islam, Hongliang Ren
+Note            : Code adopted and 
