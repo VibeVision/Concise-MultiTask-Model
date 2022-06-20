ec232ea67a355180e145a56f21026fe75d40bf16
VibeVision <VibeVisio80@proton.me>
2022-06-20 12:05:25 +0000

    new notes

--- utils/scene_graph_eval_matrix.py
+++ utils/scene_graph_eval_matrix.py
@@ -1,4 +1,9 @@
 import numpy as np
 import sklearn.metrics
 
-def compute
+def compute_mean_avg_prec(y_true, y_score):
+    try:
+        avg_prec = sklearn.metrics.average_precision_score(y_true, y_score, average=None)
+        mean_avg_prec = np.nansum(avg_prec) / len(avg_prec)
+    except ValueError:
+        mean_a
