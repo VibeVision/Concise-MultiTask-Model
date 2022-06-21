556ce696c4e8adc1d03f663ea2b7dfd7b87a0925
VibeVision <VibeVisio80@proton.me>
2022-06-21 11:47:53 +0000

    Version 1.8.2

--- utils/segmentation_eval_matrix.py
+++ utils/segmentation_eval_matrix.py
@@ -1,4 +1,17 @@
 import numpy as np
 
 import torch
-import torch.nn as
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+
+def batch_pix_accuracy(output, target):
+    """Batch Pixel Accuracy
+    Args:
+        predict: input 4D tensor
+        target: label 3D tensor
+    """
+    _, predict = torch.max(output, 1)
+
+    predi
