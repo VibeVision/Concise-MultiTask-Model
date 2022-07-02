3acec985b2d01d636e087586862057f36833182a
VibeVision <VibeVisio80@proton.me>
2022-07-02 09:42:25 +0000

    add file upload

--- utils/segmentation_eval_matrix.py
+++ utils/segmentation_eval_matrix.py
@@ -39,4 +39,8 @@ def batch_intersection_union(output, target, nclass):
     target = target.cpu().numpy().astype('int64') + 1
 
     predict = predict * (target > 0).astype(predict.dtype)
-    intersection = predict * (predict == ta
+    intersection = predict * (predict == target)
+    # areas of intersection and union
+    area_inter, _ = np.histogram(intersection, bins=nbins, range=(mini, maxi))
+    area_pred, _ = np.histogram(predict, bins=nbins, range=(mini, maxi))
+    area_lab, _ = np.histogram(target, bins=nbins, 
