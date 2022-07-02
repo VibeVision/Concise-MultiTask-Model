f8a874b7e218006d8271cea36b5ccc6c45fbaa84
VibeVision <VibeVisio80@proton.me>
2022-07-02 13:53:44 +0000

    Adding more snippets

--- utils/segmentation_eval_matrix.py
+++ utils/segmentation_eval_matrix.py
@@ -43,4 +43,11 @@ def batch_intersection_union(output, target, nclass):
     # areas of intersection and union
     area_inter, _ = np.histogram(intersection, bins=nbins, range=(mini, maxi))
     area_pred, _ = np.histogram(predict, bins=nbins, range=(mini, maxi))
-    area_lab, _ = np.histogram(target, bins=nbins, 
+    area_lab, _ = np.histogram(target, bins=nbins, range=(mini, maxi))
+    area_union = area_pred + area_lab - area_inter
+    assert (area_inter <= area_union).all(), \
+        "Intersection area should be smaller than Union area"
+    return area_inter, area_union
+
+
+class SegmentationLosse
