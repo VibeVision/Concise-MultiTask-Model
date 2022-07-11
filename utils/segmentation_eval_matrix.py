md: fix calculation of stacking limits on level change.

utils/segmentation_eval_matrix.py
@@ -65,4 +65,7 @@ class SegmentationLosses(nn.CrossEntropyLoss):
     def forward(self, *inputs):
         if not self.se_loss and not self.aux:
             return super(SegmentationLosses, self).forward(*inputs)
-        elif not self.
+        elif not self.se_loss:
+            pred1, pred2, target = tuple(inputs)
+            loss1 = super(SegmentationLosses, self).forward(pred1, target)
+            loss2 = super(SegmentationLosses, self).forward(pred2, targe