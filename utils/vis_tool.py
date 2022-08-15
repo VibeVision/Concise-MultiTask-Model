7ba69f3a2a822cba615f98b9f141f180a8898d18
VibeVision <VibeVisio80@proton.me>
2022-08-15 11:50:32 +0000

    Typing

--- utils/vis_tool.py
+++ utils/vis_tool.py
@@ -9,3 +9,10 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plot
 from PIL import Image, ImageDraw, ImageFont
 
+
+def vis_img(img, node_classes, bboxs,  det_action, data_const, score_thresh = 0.7):
+    
+    Drawer = ImageDraw.Draw(img)
+    line_width = 3
+    outline = '#FF0000'
+    font = ImageFont.truetype(font='/usr/share/fonts/truetype/f
